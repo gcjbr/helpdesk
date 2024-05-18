a432b00bf64454c76e00131f07ec2ba9449d40d2
Gustavo Caetano <souljacker@gmail.com>
2024-05-18 16:37:24 -0300

    Add loading and suspense

---
+++ app/(dashboard)/tickets/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <p className="text-xl text-primary">Loading...</p>
+      </div>
+    </main>
+  );
+}
